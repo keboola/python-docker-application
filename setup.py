69b0b68e6cca88a6761229ff53aa9f5a4a84af85
Odin <odinuv@gmail.com>
2015-12-08 18:58:27 +0100

    fix: setup file modules

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from distutils.core import setup
 setup(
     name='Keboola',
     version='1.0',
-    url='https://github.com/keboola/python-docker-application'
+    url='https://github.com/keboola/python-docker-application',
+    py_modules=['Keboola', 'Keboola.docker'],
 )
