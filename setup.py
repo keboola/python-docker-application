c21b5698b86f218d8117317c68ca9e0fd85d7503
Odin <odinuv@gmail.com>
2015-12-08 19:03:01 +0100

    fix: setup file modules

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='Keboola',
     version='1.0',
     url='https://github.com/keboola/python-docker-application',
-    packages=['keboola', 'keboola.docker'],
+    packages=['keboola'],
 )
